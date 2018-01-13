17733f6f269567d3ef1bf8b2a807b558b43a33a4
Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>
2018-01-13 13:11:32 -0600

    * Included PATH variable in the script to facilitate use of various tools by cron job * use git pull if repo already exist instead cloning each time * Added few messages to make output understandable :)
    Signed-off-by: Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>

--- deb-packaging.sh
+++ deb-packaging.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 clean(){
   #rm -Rf sqlitebrowser-deb-packaging
-  rm -Rf sqlitebrowser
+  #rm -Rf sqlitebrowser
   rm -Rf build
 }
 
@@ -26,12 +27,32 @@ commitCount(){
 clean #remove existing data
 working_dir="$(pwd)"
 majorversion="3.10.100ubuntu1-0"
-declare -a  distros=("xenial" "zesty" "artful" "bionic")
-#git clone https://github.com/deepsidhu1313/sqlitebrowser-deb-packaging.git
-git clone https://github.com/sqlitebrowser/sqlitebrowser.git
-echo "${working_dir}"
+declare -a  distros=("xenial" "zesty" "artful" "bionic") # distributions codenames
+if [ -d "${working_dir}/sqlitebrowser-deb-packaging" ]
+then
+  echo "deb packing git repo found under ${working_dir}/sqlitebrowser-deb-packaging"
+  cd "${working_dir}/sqlitebrowser-deb-packaging"
+  echo "pulling latest code"
+  git pull
+else
+  echo "Warning! deb packing git repo not found under ${working_dir}/sqlitebrowser-deb-packaging"
+  echo "Info! cloning https://github.com/deepsidhu1313/sqlitebrowser-deb-packaging.git to ${working_dir}/sqlitebrowser-deb-packaging"
+  git clone https://github.com/deepsidhu1313/sqlitebrowser-deb-packaging.git
+fi
+cd "${working_dir}"
+if [ -d "${working_dir}/sqlitebrowser" ]
+then
+  echo "sqlitebrowser git repo found under ${working_dir}/sqlitebrowser"
+  cd "${working_dir}/sqlitebrowser"
+  echo "pulling latest code"
+  git pull
+else
+  echo "Warning! DB4S git repo not found under ${working_dir}/sqlitebrowser"
+  echo "Info! cloning https://github.com/sqlitebrowser/sqlitebrowser.git to ${working_dir}/sqlitebrowser"
+  git clone https://github.com/sqlitebrowser/sqlitebrowser.git
+fi
 if [ ! -f "${working_dir}/lastrevno" ]; then
-  echo "File not found!"
+  echo "Last Revision number on master branch not found! Considering 0 for start"
   echo '0' > "${working_dir}/lastrevno"
 fi
 cd "${working_dir}/sqlitebrowser"
@@ -40,21 +61,18 @@ pathtolastrevno="${working_dir}/lastrevno"
 pathtorevno="${working_dir}/revno"
 lastrevno=$(cat "$pathtolastrevno")
 revno=$(cat "$pathtorevno")
-echo $pathtolastrevno
-echo $lastrevno
 version0="${revno}~"
 version=$version0$(timestamp)
-timestamp
 if [[ "${revno}" -gt "${lastrevno}" ]]; then
-  echo "Code changed"
+  echo "Code changed Latest Revision number on master branch is $revno"
 
-for i in "${distros[@]}"
-do
-  cd "${working_dir}/"
-  mkdir -p "${working_dir}/build/$i/sqlitebrowser/"
-  rsync -aAX sqlitebrowser-deb-packaging/ "build/$i/sqlitebrowser/" --exclude='.git'
-  rsync -aAX sqlitebrowser/ "build/$i/sqlitebrowser/" --exclude='.git'
-  cd "${working_dir}/build/$i/sqlitebrowser/"
+  for i in "${distros[@]}"
+  do
+    cd "${working_dir}/"
+    mkdir -p "${working_dir}/build/$i/sqlitebrowser/"
+    rsync -aAX sqlitebrowser-deb-packaging/ "build/$i/sqlitebrowser/" --exclude='.git'
+    rsync -aAX sqlitebrowser/ "build/$i/sqlitebrowser/" --exclude='.git'
+    cd "${working_dir}/build/$i/sqlitebrowser/"
     echo "sqlitebrowser ($majorversion~${version}~$i) $i; urgency=high
 
   * Nightly Build
@@ -71,9 +89,9 @@ do
     # upload packages to ppa
     dput ppa:linuxgndu/sqlitebrowser-testing sqlitebrowser_*source.changes
 
-  cd "${working_dir}"
+    cd "${working_dir}"
 
-done
-# change last revision number
-echo "${revno}" > "${working_dir}/lastrevno"
+  done
+  # change last revision number
+  echo "${revno}" > "${working_dir}/lastrevno"
 fi
